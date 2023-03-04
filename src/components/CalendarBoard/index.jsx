e098f336cd179b0d169a1e80f4beb72cf530295f
t-tazy <takumin390511@gmail.com>
2023-03-04 16:01:57 +0900

    feat: 曜日を表示・1日は月情報も表示
    re #1

--- src/components/CalendarBoard/index.jsx
+++ src/components/CalendarBoard/index.jsx
@@ -1,12 +1,27 @@
+import { Typography } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { createCalendar } from '../../services/calendar';
 import { CalendarElement } from '../CalendarElement';
 import './style.css';
 
 const calendarArray = createCalendar();
+const days = ['日', '月', '火', '水', '木', '金', '土'];
 
 export const CalendarBoard = () => (
   <Grid2 container className="grid" columns={7} spacing={0}>
+    {days.map((d) => (
+      <Grid2 xs={1}>
+        <Typography
+          className="days"
+          color="textSecondary"
+          align="center"
+          variant="caption"
+          component="div"
+        >
+          {d}
+        </Typography>
+      </Grid2>
+    ))}
     {calendarArray.map((day) => (
       <Grid2 key={day.toISOString()} xs={1}>
         <CalendarElement day={day} />
--- src/components/CalendarElement/index.jsx
+++ src/components/CalendarElement/index.jsx
@@ -1,15 +1,20 @@
 import { Typography } from '@mui/material';
 import './style.css';
 
-export const CalendarElement = ({ day }) => (
-  <div className="element">
-    <Typography
-      className="date"
-      align="center"
-      variant="caption"
-      component="div"
-    >
-      {day.format('D')}
-    </Typography>
-  </div>
-);
+export const CalendarElement = ({ day }) => {
+  const isFirstDay = day.date() === 1;
+  const format = isFirstDay ? 'M月D日' : 'D';
+
+  return (
+    <div className="element">
+      <Typography
+        className="date"
+        align="center"
+        variant="caption"
+        component="div"
+      >
+        {day.format(format)}
+      </Typography>
+    </div>
+  );
+};
