e6d7898b57af8acf3ee73a1e6c14e221099e7a7a
t-tazy <takumin390511@gmail.com>
2023-03-15 23:28:00 +0900

    feat: AddRecordDialogを開閉できるように実装
    Dialogの中身は未実装
    
    re #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
+import { ContainerAddRecordDialog } from './components/AddRecordDialog/container';
 import { ContainerCalendarBoard } from './components/CalendarBoard/container';
 import { ContainerNavigation } from './components/Navigation/container';
 
 export const App = () => {
+  const [addRecordDialog, setAddRecordDialog] = useState({
+    form: {},
+    isOpen: false,
+  });
+
+  // Dialogを開閉する
+  const toggleDialog = () => {
+    setAddRecordDialog({
+      ...addRecordDialog,
+      isOpen: !addRecordDialog.isOpen,
+    });
+  };
+
   return (
     <div className="App">
       <ContainerNavigation />
-      <ContainerCalendarBoard />
+      <ContainerCalendarBoard toggleAddRecordDialog={toggleDialog} />
+      <ContainerAddRecordDialog
+        dialog={addRecordDialog}
+        toggleAddRecordDialog={toggleDialog}
+      />
     </div>
   );
 };
