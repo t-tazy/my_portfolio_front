883ca3b50fe0ef91e56f47383e1dc4efc6abb65c
t-tazy <takumin390511@gmail.com>
2023-03-20 22:58:01 +0900

    feat: エクササイズ一覧取得処理を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import { ContainerAddRecordDialog } from './components/AddRecordDialog/container';
 import { ContainerCalendarBoard } from './components/CalendarBoard/container';
 import { ContainerNavigation } from './components/Navigation/container';
+import { URL } from './const';
 
 export const App = () => {
   const [addRecordDialog, setAddRecordDialog] = useState({
     form: {},
     isOpen: false,
   });
+  const [exercises, setExercises] = useState();
+  const [cookies] = useCookies();
 
   // Dialogを開閉する
   const toggleDialog = () => {
@@ -16,6 +20,21 @@ export const App = () => {
       isOpen: !addRecordDialog.isOpen,
     });
   };
+
+  // エクササイズ一覧を取得
+  const fetchExercise = async () => {
+    const rsp = await fetch(`${URL}/exercises`, {
+      headers: { authorization: `Bearer ${cookies.token}` },
+    });
+    const data = await rsp.json();
+    setExercises(data.map((val) => val.title))
+    console.log(data);
+  };
+
+  useEffect(() => {
+    console.log('mount');
+    fetchExercise();
+  }, []);
 
   return (
     <div className="App">
